fa2748ee9ad073d65ca6eb45f2700069f80afeb0
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-28 16:55:08 +0200

    style inserito

--- vue/hello.js
+++ vue/hello.js
@@ -4,7 +4,10 @@ createApp({
     data() {
         return {
             message: 'Benvenuti su Vue.js!',
-            image: '/img/dragon-ball-super-gohan.webp'
+            image: '/img/dragon-ball-super-gohan.webp',
+            image2:'/img/por-cierto-calamardo-de-bob-esponja-no-es-un-cala-2-1245-1508437390-1_dblbig.jpg',
+            image3:'/img/majin-boo_jebw.jpg',
+            myclass: "img-fluid rounded"
         }
         
       }
